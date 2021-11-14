1bea99b17fb61649ae6fdf61fd03940986ab4bb0
Sir Com <lars.pfleiderfb@gmail.com>
2021-11-14 12:32:11 +0100

    4space to tab

--- pinchBlue_adapter.c
+++ pinchBlue_adapter.c
@@ -26,40 +26,40 @@ void pinchBlue_adapter_thread(	int 				dev_id,
 								pthread_mutex_t* 	mutex, 
 								plist 				data[3])
 {
-    int x = 0;
-    while (pinchBlue_adapter_checkTermination(keepRunning, mutex))
-    {
-        pthread_mutex_lock(data[0].mutex);
-        x = pinchBlue_plist_count(&data[0]);
-        pthread_mutex_unlock(data[0].mutex);
-        if (x)
-        {
-            pinchBlue_adapter_imitate(dev_id, socket, data);
-        }
-        else 
-        {
-            pinchBlue_adapter_search(dev_id, socket, data);
-        }
-    }
+	int x = 0;
+	while (pinchBlue_adapter_checkTermination(keepRunning, mutex))
+	{
+		pthread_mutex_lock(data[0].mutex);
+		x = pinchBlue_plist_count(&data[0]);
+		pthread_mutex_unlock(data[0].mutex);
+		if (x)
+		{
+			pinchBlue_adapter_imitate(dev_id, socket, data);
+		}
+		else 
+		{
+			pinchBlue_adapter_search(dev_id, socket, data);
+		}
+	}
 }
 
 int pinchBlue_adapter_checkTermination(int* keepRunning, pthread_mutex_t* mutex)
 {
-    pthread_mutex_lock(mutex);
-    int out = *keepRunning;
-    pthread_mutex_unlock(mutex);
-    return out;
+	pthread_mutex_lock(mutex);
+	int out = *keepRunning;
+	pthread_mutex_unlock(mutex);
+	return out;
 }
 
 void pinchBlue_adapter_imitate(int dev_id, int socket, plist data[3])
 {
-    xdevice* externalDevice = pinchBlue_pList_pop(&data[0]);
+	xdevice* externalDevice = pinchBlue_pList_pop(&data[0]);
 
-    pinchBlue_plist_addElem(externalDevice, &data[1]);
+	pinchBlue_plist_addElem(externalDevice, &data[1]);
 }
 
 void pinchBlue_adapter_search(int dev_id, int socket, plist data[3])
 {
-    inquiry_info* ii = NULL;
-    hci_inquiry(dev_id, 1, 255, NULL, &ii, NULL);
+	inquiry_info* ii = NULL;
+	hci_inquiry(dev_id, 1, 255, NULL, &ii, NULL);
 }
--- pinchBlue_plist.c
+++ pinchBlue_plist.c
@@ -4,138 +4,138 @@
 
 plist* pinchBlue_plist_createPlist()
 {
-    plist* out = (plist*)malloc(sizeof(plist));
-    if(!out)
-        return NULL;
-    if (pthread_mutex_init(out->mutex, NULL))
-    {
-        free(out);
-        return NULL;
-    }
-    out->start = NULL;
-    return out;
+	plist* out = (plist*)malloc(sizeof(plist));
+	if(!out)
+		return NULL;
+	if (pthread_mutex_init(out->mutex, NULL))
+	{
+		free(out);
+		return NULL;
+	}
+	out->start = NULL;
+	return out;
 }
 
 elem* pinchBlue_plist_createElem(void* data)
 {
-    elem* out = (elem*)malloc(sizeof(elem));
-    if (out)
-    {
-        out->data = data;
-        out->next = NULL;
-    }
-    return out;
+	elem* out = (elem*)malloc(sizeof(elem));
+	if (out)
+	{
+		out->data = data;
+		out->next = NULL;
+	}
+	return out;
 }
 
 void pinchBlue_plist_addElem(void* data, plist* l)
 {
-    if (l->start)
-    {
-        elem* current = l->start;
-        while (current->next)
-            current = current->next;
-        current->next = pinchBlue_plist_createElem(data);
-    }
-    else l->start = pinchBlue_plist_createElem(data);
+	if (l->start)
+	{
+		elem* current = l->start;
+		while (current->next)
+			current = current->next;
+		current->next = pinchBlue_plist_createElem(data);
+	}
+	else l->start = pinchBlue_plist_createElem(data);
 }
 
 void pinchBlue_plist_removeElem(int index, plist* l)
 {
-    if (pinchBlue_plist_count(l) <= index)
-        return;
-    if (index == 0)
-    {
-        elem* current = l->start;
-        l->start = current->next;
-        free(current->data);
-        current->data = NULL;
-        current->next = NULL;
-        free(current);
-    }
-    else
-    {
-        int x = 0;
-        elem* current = l->start;
-        elem* next = current-> next;    
-        while (x != index - 1)
-        {
-            next = next->next;
-            current = current->next;
-            x++;
-        }
-        current->next = current->next->next;
-        free(next->data);
-        next->data = NULL;
-        next->next = NULL;
-        free(next);
-    }
+	if (pinchBlue_plist_count(l) <= index)
+		return;
+	if (index == 0)
+	{
+		elem* current = l->start;
+		l->start = current->next;
+		free(current->data);
+		current->data = NULL;
+		current->next = NULL;
+		free(current);
+	}
+	else
+	{
+		int x = 0;
+		elem* current = l->start;
+		elem* next = current-> next;	
+		while (x != index - 1)
+		{
+			next = next->next;
+			current = current->next;
+			x++;
+		}
+		current->next = current->next->next;
+		free(next->data);
+		next->data = NULL;
+		next->next = NULL;
+		free(next);
+	}
 }
 
 void pinchBlue_plist_removeLastElem(plist* l)
 {
-    elem* current = l->start;
-    if (current)
-    {
-        if(current->next)
-        {
-            while (current->next->next)
-                current = current->next;
-            free(current->next);
-            current->next = NULL;
-        }
-        else
-        {
-            free(current->data);
-            free(current);
-            l->start = NULL;
-        }
-    }
+	elem* current = l->start;
+	if (current)
+	{
+		if(current->next)
+		{
+			while (current->next->next)
+				current = current->next;
+			free(current->next);
+			current->next = NULL;
+		}
+		else
+		{
+			free(current->data);
+			free(current);
+			l->start = NULL;
+		}
+	}
 }
 
 int pinchBlue_plist_count(plist* l)
 {
-    int i = 0;
-    elem* current = l->start;
-    while (current)
-    {
-        i++;
-        current = current->next;
-    } 
-    return i;
+	int i = 0;
+	elem* current = l->start;
+	while (current)
+	{
+		i++;
+		current = current->next;
+	} 
+	return i;
 }
 
 void* pinchBlue_plist_get(int index, plist* l)
 {
-    int x = 0;
-    elem* current = l->start;
-    while(current && x < index)
-    {
-        current = current->next;
-        x++;
-    }
-    return (current) ? current->data : NULL;
+	int x = 0;
+	elem* current = l->start;
+	while(current && x < index)
+	{
+		current = current->next;
+		x++;
+	}
+	return (current) ? current->data : NULL;
 }
 
 void* pinchBlue_plist_getLast(plist* l)
 {
-    elem* current = l->start;
-    while(current)
-    {
-        if(current->next)
-            current = current->next;
-        else break;
-    }
-    return (current) ? current->data : NULL;
+	elem* current = l->start;
+	while(current)
+	{
+		if(current->next)
+			current = current->next;
+		else break;
+	}
+	return (current) ? current->data : NULL;
 }
 
 void* pinchBlue_plist_getFirst(plist* l)
 {
-    return (l->start) ? l->start->data : NULL;
+	return (l->start) ? l->start->data : NULL;
 }
 
 void* pinchBlue_plist_pop(plist* l)
 {
-    void* out = pinchBlue_plist_getLast(l);
-    pinchBlue_plist_removeLastElem(l);
-    return out;
+	void* out = pinchBlue_plist_getLast(l);
+	pinchBlue_plist_removeLastElem(l);
+	return out;
 }
